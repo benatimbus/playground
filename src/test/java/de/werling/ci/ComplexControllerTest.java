230db8e9737d54dc4ef0b656a8d282d30580995e
User <User@DESKTOP-93I3BEB>
2018-08-11 17:16:41 +0200

    extended tests

--- src/test/java/de/werling/ci/ComplexControllerTest.java
+++ src/test/java/de/werling/ci/ComplexControllerTest.java
@@ -24,7 +24,26 @@ public class ComplexControllerTest {
 
     @Test
     public void shouldReturn4If3and1IsAdded() throws Exception {
-        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/api/calculate?left=1&right=3&op=+",
-                int.class)).isEqualTo(4);
+        assertThat(sendRequest("1", "3", '+', int.class)).isEqualTo(4);
+    }
+
+    @Test
+    public void shouldReturn14If7and2AreMultiplied() throws Exception {
+        assertThat(sendRequest("7", "2", '*', int.class)).isEqualTo(14);
+    }
+
+    @Test
+    public void shouldReturn2If3andMinus1IsAdded() throws Exception {
+        assertThat(sendRequest("-1", "3", '+', int.class)).isEqualTo(2);
+    }
+
+    @Test
+    public void shouldReturn4If8OIsDividedBy2() throws Exception {
+        assertThat(sendRequest("8", "2", '/', int.class)).isEqualTo(4);
+    }
+
+    private <T> T sendRequest(String left, String right, char op, Class<T> resultType){
+        return this.restTemplate.getForObject("http://localhost:" + port + "/api/calculate?left="+left+"&right="+right+"&op="                +op,
+                resultType);
     }
 }
